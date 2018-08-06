5875ee2d89974918f1b3f2f4c9af7a661bb271b8
Ramirez Romo Daniel <ramirezrd2018@dci25-159.ugto.mx>
2018-08-06 10:00:27 -0500

    Añadir archivo

---
+++ semana1/programa_hola.c
@@ -0,0 +1,5 @@
+/*
+ Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por Daniel Ramírez Romo.
+*/
