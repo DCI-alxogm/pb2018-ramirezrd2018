71f89f0e917a4517f5745fadfb83d25c72e4fc77
Ramirez Romo Daniel <ramirezrd2018@dci25-159.ugto.mx>
2018-08-08 09:32:15 -0500

    Cree mi primer programa.

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -3,3 +3,13 @@
  Este programa imprime la típica frase “hola mundo a la pantalla”
  Creado el 06/08/2018 por Daniel Ramírez Romo.
 */
+/*
+Creado el 8 de Agosto del 2018 por Daniel Ramírez Romo*/
+#include<stdio.h>
+int main()
+{
+printf("Hola Daniel \n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
+
