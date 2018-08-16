0bf84f9e52a5c101f5239db274956d0138620354
Ramirez Romo Daniel <ramirezrd2018@dci25-159.ugto.mx>
2018-08-16 14:48:49 -0500

    Ejercicio 1 de la semana 2

---
+++ semana2/primer_programa.c
@@ -0,0 +1,15 @@
+/*Creado el 15 de Agosto de 2018 por Daniel Ramirez Romo*/
+#include<stdio.h>
+int main()
+{
+int edad;
+
+printf("Hola Daniel \n");
+printf("Este es el 1er programa del curso\n");
+printf("Introduce tu edad: \n");
+scanf("%i",&edad);
+edad +=10;
+printf("\ntu edad en 2028 será: %i\n", edad);
+return 0;
+
+}
